8510be91aaa62086efe15f9c679f57b814dfe639
Davi Lemes <ndlemes7@outlook.com>
2023-08-10 08:08:06 -0300

    Pessoa e Banda criadas, atributos, construtor e métodos

---
+++ Servidor/src/Banda.java
@@ -0,0 +1,29 @@
+public class Banda {
+    private String nome;
+    private int qntdIntegrantes;
+    private int anoLacamentoPrimeiraMusica;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQntdIntegrantes() {
+        return qntdIntegrantes;
+    }
+
+    public void setQntdIntegrantes(int qntdIntegrantes) {
+        this.qntdIntegrantes = qntdIntegrantes;
+    }
+
+    public int getAnoLacamentoPrimeiraMusica() {
+        return anoLacamentoPrimeiraMusica;
+    }
+
+    public void setAnoLacamentoPrimeiraMusica(int anoLacamentoPrimeiraMusica) {
+        this.anoLacamentoPrimeiraMusica = anoLacamentoPrimeiraMusica;
+    }
+}
---
+++ Servidor/src/Pessoa.java
@@ -0,0 +1,29 @@
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private String endereco;
+
+    public Pessoa(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
