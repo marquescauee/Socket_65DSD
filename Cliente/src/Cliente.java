68371c1badf50ac7dc3745e9f4f9c0fd2356d878
marquescauee <cauelopesmarques123@gmail.com>
2023-08-10 22:07:01 -0300

    Client Fix

--- Cliente/src/Cliente.java
+++ Cliente/src/Cliente.java
@@ -3,30 +3,37 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Cliente {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		
-		System.out.println("Criando conex√£o");
-
-		try (Socket conn = new Socket("192.168.1.27", 80);) {
+		System.out.println("Criando conex„o...");
+		
+		System.out.println("Digite a opÁ„o:");
+		Scanner s = new Scanner(System.in);
+		String msg = s.next();
+		
+		try (Socket conn = new Socket("10.15.120.70", 80);) {
 			System.out.println("Conectado!");
-			//InputStream in = conn.getInputStream();
-			OutputStream out = conn.getOutputStream();
+			InputStream in = conn.getInputStream();
 			
-//			byte[] dadosBrutos = new byte[1024];
-//			int qtdBytesLidos = in.read(dadosBrutos);
-//			while(qtdBytesLidos >= 0) {
-//				String dadosStr = new String(dadosBrutos, 0, qtdBytesLidos);
-//				System.out.println(dadosStr);
-//				qtdBytesLidos = in.read(dadosBrutos);
-//			}
-			String msg = "Vou enviar a opera√ß√£o de salvar uma pessoa";
+			OutputStream out = conn.getOutputStream();
 			out.write(msg.getBytes());
+			
+			byte[] dadosBrutos = new byte[1024];
+			int qtdDadosBrutos = in.read(dadosBrutos);
+			while(qtdDadosBrutos >= 0) {
+				String stringDados = new String(dadosBrutos, 0, qtdDadosBrutos);
+				System.out.println(stringDados);
+				qtdDadosBrutos = in.read(dadosBrutos);
+			}
 		} catch(UnknownHostException e) {
-			System.out.println("Host n√£o encontrado");
+			System.out.println("Host n„o encontrado");
 			e.printStackTrace();
+		} finally {
+			s.close();
 		}
 	}
 
