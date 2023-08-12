2aba6e115fe24822edaf8d4117fd72b5e09c00c4
marquescauee <cauelopesmarques123@gmail.com>
2023-08-12 00:09:42 -0300

    Client Updated

--- Cliente/src/Cliente.java
+++ Cliente/src/Cliente.java
@@ -12,14 +12,26 @@ public class Cliente {
 		String opcao = "";
 		Scanner s = new Scanner(System.in);
 		while (!opcao.equalsIgnoreCase("sair")) {
-			System.out.println("Digite a opção desejada no seguinte formato:");
+			System.out.println("Digite a opÃ§Ã£o desejada no seguinte formato:");
 			System.out.println("=====================================================");
-			System.out.println("insert;cpf;nome;endereço \t[inserir uma nova pessoa]\n");
-			System.out.println("update;cpf;nome;endereço \t[atualizar os dados de uma pessoa]\n");
+			System.out.println("insert;cpf;nome;endereÃ§o \t[inserir uma nova pessoa]\n");
+			System.out.println("update;cpf;nome;endereÃ§o \t[atualizar os dados de uma pessoa]\n");
 			System.out.println("delete;cpf \t\t\t[remover uma pessoa]\n");
 			System.out.println("get;cpf; \t\t\t[recuperar uma pessoa]\n");
 			System.out.println("list \t\t\t\t[recuperar todas as pessoas]\n");
-			System.out.println("sair \t\t\t\t[encerra a aplicação]\n");
+
+			System.out.println("=====================================================");
+			System.out.println("insertBanda;nome;qtdIntegrantes;anoPrimeiraMusica \t[inserir uma nova banda]\n");
+			System.out.println("updateBanda;nome;qtdIntegrantes;anoPrimeiraMusica \t[atualizar os dados de uma banda]\n");
+			System.out.println("delete;nome \t\t\t\t\t\t[remover uma banda]\n");
+			System.out.println("insertIntegrante;nomeBanda;cpf \t\t\t\t[adicionar integrante a uma banda]\n");
+			System.out.println("insertIntegrante;nomeBanda;cpf \t\t\t\t[remover integrante de uma banda]\n");
+			System.out.println("get;nome; \t\t\t\t\t\t[recuperar uma banda]\n");
+			System.out.println("list \t\t\t\t\t\t\t[listar todas as bandas]\n");
+			System.out.println("=====================================================");
+			
+			
+			System.out.println("sair \t\t[encerrar a aplicaÃ§Ã£o]\n");
 
 		
 			opcao = s.next();
@@ -41,7 +53,7 @@ public class Cliente {
 				}
 				opcao = s.nextLine();
 			} catch (UnknownHostException e) {
-				System.out.println("Host não encontrado");
+				System.out.println("Host nï¿½o encontrado");
 				e.printStackTrace();
 			}
 		}
